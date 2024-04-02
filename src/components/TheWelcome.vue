b98bc227f6cbefac36bad1e33936e499fe42ca2e
gabrielmartinsgomes <gabrielhenrickmarind3454@gmail.com>
2024-04-02 14:56:32 -0300

    contatos

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,21 +5,29 @@
     <img src="/img/lari.jpeg" />
     <div class="bio">
       <h1>Larissa Lotin</h1>
-      <p>16 anos, 139 de altura</p>
+      <p>16 anos</p>
+      <a href="https://www.linkedin.com/in/larissa-lotin-46997429a/?originalSubdomain=br">LinkedIn</a> |
+      <a href="https://github.com/lotinn">Github</a>
     </div>
   </div>
   <div class="membros">
     <img src="/img/gb.jpeg" />
     <div class="bio">
       <h1>Gabriel Gomes</h1>
-      <p>16 anos, 139 de altura</p>
+      <p>16 anos</p>
+      <a href="https://www.linkedin.com/in/gabriel-henrick-martins-gomes-27230b299/">LinkedIn</a> |
+      <a href="https://github.com/gabrielmartinsgomes">GitHub</a>
     </div>
   </div>
   <div class="membros">
     <img src="/img/murilo.jpg" />
     <div class="bio">
       <h1>Murilo Souza</h1>
-      <p>16 anos, 139 de altura</p>
+      <p>16 anos</p>
+      <div class="links">
+      <a href="https://www.linkedin.com/in/murilo-da-cruz-souza-439809299/">LinkedIn</a> |
+      <a href="https://github.com/MuriloDaCruz">GitHub</a>
+      </div>
     </div>
   </div>
 </template>
