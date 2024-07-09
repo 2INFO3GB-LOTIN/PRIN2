eda17ab9256e20199183f2936501e6512a5cce2f
gabrielmartinsgomes <gabrielhenrickmarind3454@gmail.com>
2024-07-09 14:04:33 -0300

    ebraim

--- src/components/ContatosInfo.vue
+++ src/components/ContatosInfo.vue
@@ -2,12 +2,14 @@
 
 <template>
   <div class="membros">
-    <img src="/img/lari.jpeg" />
+    <img src="/img/ebraim.png" />
     <div class="bio">
-      <h1>Larissa Lotin</h1>
-      <p>16 anos</p>
-      <a href="https://www.linkedin.com/in/larissa-lotin-46997429a/?originalSubdomain=br">LinkedIn</a> |
-      <a href="https://github.com/lotinn">Github</a>
+      <h1>Ebraim Herrera</h1>
+      <p>17 anos</p>
+      <div class="links">
+        <a href="https://www.linkedin.com/in/murilo-da-cruz-souza-439809299/">LinkedIn</a> |
+        <a href="https://github.com/Ebraim30">GitHub</a>
+      </div>
     </div>
   </div>
   <div class="membros">
@@ -20,13 +22,25 @@
     </div>
   </div>
   <div class="membros">
+    <img src="/img/lari.jpeg" />
+    <div class="bio">
+      <h1>Larissa Lotin</h1>
+      <p>16 anos</p>
+      <a href="https://www.linkedin.com/in/larissa-lotin-46997429a/?originalSubdomain=br"
+        >LinkedIn</a
+      >
+      |
+      <a href="https://github.com/lotinn">Github</a>
+    </div>
+  </div>
+  <div class="membros">
     <img src="/img/murilo.jpg" />
     <div class="bio">
       <h1>Murilo Souza</h1>
       <p>16 anos</p>
       <div class="links">
-      <a href="https://www.linkedin.com/in/murilo-da-cruz-souza-439809299/">LinkedIn</a> |
-      <a href="https://github.com/MuriloDaCruz">GitHub</a>
+        <a href="https://www.linkedin.com/in/murilo-da-cruz-souza-439809299/">LinkedIn</a> |
+        <a href="https://github.com/MuriloDaCruz">GitHub</a>
       </div>
     </div>
   </div>
